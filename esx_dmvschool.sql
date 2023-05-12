0f6b043cb802a6652be0c8fde7b621b5b538733d
Manu06DE <109236239+Manu06DE@users.noreply.github.com>
2023-05-12 23:10:54 +0200

    Add files via upload

---
+++ esx_dmvschool.sql
@@ -0,0 +1,8 @@
+INSERT INTO `licenses` (`type`, `label`) VALUES
+	('dmv', 'Theoriebescheinigung'),
+	('drive', 'Klasse B'),
+	('drive_bike', 'Klasse A'),
+	('drive_truck', 'Klasse C'),
+	('drive_truck_hvy', 'Klasse CE'),
+	('drive_bus', 'Klasse D')
+;
---
+++ html/questions.js
@@ -0,0 +1,91 @@
+var tableauQuestion = [
+	{
+		question: "Trotz vorhandenen Gehwegs auf der rechten Straßenseite fährt eine Person im Rollstuhl auf der Straße. Wie verhalten sie sich?",
+		propositionA: "Ich mache die Person durch Hupen darauf aufmerksam, dass sie sich falsch verhält.",
+		propositionB: "Ich halte an und renne ihm hinterher, und schiebe ihn au den Gehweg, damit die straße wieder frei ist.",
+		propositionC: "Ich fahre so lange hinterher, bis ich überholen kann.",
+		propositionD: "Ich beschleunige bis ich ihm auffahre.",
+		reponse: "C"
+	},
+
+	{
+		question: "Was kann schon durch eine 20 prozentige Überladung eintreten?",
+		propositionA: "Verschlechtung des Lenkverhaltens",
+		propositionB: "Überbeansprunchung der Bremsen",
+		propositionC: "Schäden an tragenden Fahrzeugteilen",
+		propositionD: "Alles Oben genannte",
+		reponse: "D"
+	},
+
+	{
+		question: "Ohne vorherige Angabe ist die Geschwindigkeit in einem Wohngebiet: __ km/h",
+		propositionA: "30 km/h",
+		propositionB: "50 km/h",
+		propositionC: "40 km/h",
+		propositionD: "60 km/h",
+		reponse: "B"
+	},
+
+	{
+		question: "Woran erkennen Sie den kürzesten Weg zur nächsten Notrufsäule auf Bundesautobahnen? An den-",
+		propositionA: "- schwarzen Pfeilen auf den Leitpfosten",
+		propositionB: "- Bodenmarkierungen auf dem Standstreifen",
+		propositionC: "- Richtungsmakierungen auf dem Leitplanken",
+		propositionD: "- Straßenmakierungen auf der Fahrbahn",
+		reponse: "A"
+	},
+
+	{
+		question: "Unter Welchen Umständen dürfen Sie an einer Kreuzung eine Rote Ampel überfahren?",
+		propositionA: "Wenn ein Polizist den Verkehr auf der Keuzung regelt",
+		propositionB: "Unter keinen Umständen",
+		propositionC: "Um einem Einsatzfahrzeug mit Blaumen Blinklicht und Einsatzhorn vorsichtig Platz zu schaffen",
+		propositionD: "Unter den 2 Umständen",
+		reponse: "D"
+	},
+
+	{
+		question: "Wozu kann eine Plötzliche Verschlechterung des Fahrbahnzustandes führen?",
+		propositionA: "Zu veränderten Reifengeräuschen, Schleuder- und Ruschgefahr",
+		propositionB: "Ein schnellerer Bremsweg",
+		propositionC: "Zu verbesserung der Lenkung",
+		propositionD: "Zu längerem Reaktionsweg",
+		reponse: "A"
+	},
+
+	{
+		question: "Sie fahren auf einer Landstraße. Vor ihnen hat sich eine langsam fahrende Fahrzeugschlange von ca. 15 Fahrzeugen gebildet. Wie verhalten sie sich",
+		propositionA: "Ich überhole die Fahrzeugschlange schnellmöglichst",
+		propositionB: "Ich fordere Vorausfahrende duch Lichthupe und Blinken zum Überholen auf",
+		propositionC: "Ich bleibe zunächst hinter der Fahrzeugschlange",
+		propositionD: "Ich fahre fast auf, um zu signalisieren, das die Fahrzeuge schneller fahren sollen",
+		reponse: "C"
+	},
+
+	{
+		question: "Ihr beifahrer weist sie während der fahr darauf hin, dass Sie mehrfach zu dicht aufgefahren sind. Wie sollten sie sich verhalten?",
+		propositionA: "Ich ignoriere die Hinweise des Beifahrers",
+		propositionB: "Ich halte einen Größeren Abstand ein",
+		propositionC: "Ich nehme die Hinweise des Beifahrers ernst",
+		propositionD: "Ich schmeiße den Beifahrer aus dem auto, da er keine ahnung vom fahren hat",
+		reponse: "C"
+	},
+
+	{
+		question: "Wie schnell darf ein LKW mit einer zulässigen Gesamtmasse von 3,0 t und einem Anhänger auf Autobahnen höchstens fahren?",
+		propositionA: "80 km/h",
+		propositionB: "100 km/h",
+		propositionC: "50 km/h",
+		propositionD: "90 km/h",
+		reponse: "A"
+	},
+
+	{
+		question: "Wie müssen sie nachts ein Fahrzeug sichern, das an einer Stelle liegen geblieben ist, an der es eine Gefahr für den Übrigen Verkehr bildet",
+		propositionA: "Rückfahrscheinwerfer einschalten",
+		propositionB: "Warndreieck in ausreichender Entfernung aufstellen und Warnblinklicht einschalten",
+		propositionC: "Nur Warnblinklicht einschalten",
+		propositionD: "Alles gannante",
+		reponse: "B"
+	},
+]
